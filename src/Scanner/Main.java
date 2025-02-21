b51c83a21d3bdc52008ebc7da10d3b55dba44c59
Ruma <matteoh8@hotmail.it>
2025-02-21 16:14:53 +0100

    modifiche

--- src/Scanner/Main.java
+++ src/Scanner/Main.java
@@ -3,8 +3,30 @@ package Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		java.util.Scanner input;
+		input = new java.util.Scanner(System.in);
+		
+		int n1, n2, sum;
+		String s1;
+		
+		System.out.println("Digita un numero!");
+		s1 = input.nextLine();
+		n1 = Integer.parseInt(s1);
+		
+		System.out.println("Digita un secondo numero!");
+		s1 = input.nextLine();
+		n2 = Integer.parseInt(s1);
+		
+		sum = n1 + n2;
+		
+		// Usa String.format() per salvare la stringa formattata senza stamparla direttamente
+        String output = String.format("La somma tra %d e %d è %d", n1, n2, sum);
 
-	}
+        // Ora possiamo stampare l'output successivamente
+        System.out.println(output);
+
+        input.close(); // È buona pratica chiudere lo Scanner quando non serve più
+    }
 
 }
